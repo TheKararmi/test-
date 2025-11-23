2adea4e572f80dc57a7d58f1807445f0ad5b6876
Karail <karailfokus@gmail.com>
2025-11-23 17:03:10 +0300

    fix

--- pkg/model/criper.go
+++ pkg/model/criper.go
@@ -6,13 +6,13 @@ type Criper struct {
 	Spawn  *Spawn
 }
 
-func (c *Criper) MinusHP(damage int) {
-	c.HP -= damage
-}
+// func (c *Criper) MinusHP(damage int) {
+// 	c.HP -= damage
+// }
 
-func (c *Criper) GetDamage() int {
-	return c.Damage
-}
+// func (c *Criper) GetDamage() int {
+// 	return c.Damage
+// }
 
 // func (c *Criper) GetSpawn() (int, int) {
 // 	return c.Spawn.X, c.Spawn.Y
